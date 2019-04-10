ec43046a399098a67d07bbab9f29d8202dbcc99f
bmaz <beatrice.mazoyer@gmail.com>
2019-04-10 17:55:50 +0200

    docker swarm

--- src/consumer.py
+++ src/consumer.py
@@ -1,10 +1,8 @@
 from redis import Redis
 from rq import Queue, Worker
 
-
 redis = Redis(host='redis', port=6379)
 queue_out = Queue('outputs', connection=redis)
 
-# Start a worker with a custom name
-worker = Worker([queue_out], connection=redis, name='consumer')
+worker = Worker([queue_out], connection=redis)
 worker.work()
--- src/worker.py
+++ src/worker.py
@@ -1,10 +1,8 @@
 from redis import Redis
 from rq import Queue, Worker
 
-
 redis = Redis(host='redis', port=6379)
 queue = Queue('inputs', connection=redis)
 
-# Start a worker with a custom name
-worker = Worker([queue], connection=redis, name='worker')
+worker = Worker([queue], connection=redis)
 worker.work()
