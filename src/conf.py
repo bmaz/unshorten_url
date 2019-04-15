ee7166b670a7d646954c38d4334987e4e211f529
bmaz <beatrice.mazoyer@gmail.com>
2019-04-15 14:35:30 +0200

    new conf

--- src/conf.py
+++ src/conf.py
@@ -1,9 +1,7 @@
-import logging
-
-logger = ""
-file_type = "csv"
-outputfile = "tweets_urls.csv"
+outputfile = "tweets_urls_3.csv"
 proxies = {
     "http": "http://firewall.ina.fr:81",
     "https": "http://firewall.ina.fr:81",
 }
+nb_workers = 128
+date_format = "%a %b %d %H:%M:%S +0000 %Y"
