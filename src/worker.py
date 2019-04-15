1669d47702aee0f954ff206224eddf1110bd97a2
bmaz <beatrice.mazoyer@gmail.com>
2019-04-15 15:02:46 +0200

    queue default timeout

--- src/worker.py
+++ src/worker.py
@@ -2,7 +2,7 @@ from redis import Redis
 from rq import Queue, Worker
 
 redis = Redis(host='redis', port=6379)
-queue = Queue('inputs', connection=redis)
+queue = Queue('inputs', connection=redis, default_timeout=12)
 
 worker = Worker([queue], connection=redis)
 worker.work()
